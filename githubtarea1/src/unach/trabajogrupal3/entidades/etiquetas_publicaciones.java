a709f7dcdd1554619c4dfd2f8a7af1073d0568c7
Usuario <Usuario@172.30.3.243>
2018-01-11 17:46:21 -0500

    clase etiqueta

--- githubtarea1/src/unach/trabajogrupal3/entidades/etiquetas_publicaciones.java
+++ githubtarea1/src/unach/trabajogrupal3/entidades/etiquetas_publicaciones.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package unach.trabajo3.entidades;
+package unach.trabajogrupal3.entidades;
 
 /**
  *
  * @author Usuario
  */
-public class etiquetas {
+public class etiquetas_publicaciones {
     
 }
