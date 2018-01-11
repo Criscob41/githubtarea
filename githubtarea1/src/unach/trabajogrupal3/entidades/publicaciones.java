aa8f6c2c37d0a2ae7d7850838445666db0c97b71
Usuario <Usuario@172.30.3.246>
2018-01-11 17:56:09 -0500

    creacio de la clase niveles y publicaiones

---
+++ githubtarea1/src/unach/trabajogrupal3/entidades/publicaciones.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unach.trabajogrupal3.entidades;
+
+import unach.trabajo3.entidades.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class publicaciones {
+    private int id;
+    private int usuario_id;
+    private int nivel_id;
+    private String titulo;
+    private String contenido;
+    private String publicado;
+    private int vistas;
+    private int votos;
+
+    public publicaciones() {
+    }
+
+    public publicaciones(int id, int usuario_id, int nivel_id, String titulo, String contenido, String publicado, int vistas, int votos) {
+        this.id = id;
+        this.usuario_id = usuario_id;
+        this.nivel_id = nivel_id;
+        this.titulo = titulo;
+        this.contenido = contenido;
+        this.publicado = publicado;
+        this.vistas = vistas;
+        this.votos = votos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUsuario_id() {
+        return usuario_id;
+    }
+
+    public void setUsuario_id(int usuario_id) {
+        this.usuario_id = usuario_id;
+    }
+
+    public int getNivel_id() {
+        return nivel_id;
+    }
+
+    public void setNivel_id(int nivel_id) {
+        this.nivel_id = nivel_id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+
+    public String getPublicado() {
+        return publicado;
+    }
+
+    public void setPublicado(String publicado) {
+        this.publicado = publicado;
+    }
+
+    public int getVistas() {
+        return vistas;
+    }
+
+    public void setVistas(int vistas) {
+        this.vistas = vistas;
+    }
+
+    public int getVotos() {
+        return votos;
+    }
+
+    public void setVotos(int votos) {
+        this.votos = votos;
+    }
+    
+            
+}
