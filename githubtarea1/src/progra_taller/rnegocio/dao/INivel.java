ee5aa4558447ebe6ec80fe20693f7b918be93914
Fabricio <Usuario@172.30.3.144>
2018-01-16 15:01:01 -0500

    version1.2

--- githubtarea1/src/progra_taller/rnegocio/dao/INivel.java
+++ githubtarea1/src/progra_taller/rnegocio/dao/INivel.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public interface INivel {
      public int insertar(Nivel nivel) throws Exception;
+     
      public List<Nivel> obtener() throws Exception;
      
      public Nivel obtener(int codigo) throws Exception;   
