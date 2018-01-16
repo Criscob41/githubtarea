0e5fabcdc452e7c3df78cf418ac4f35c9a49d574
Fabricio <Usuario@172.30.3.144>
2018-01-16 14:43:45 -0500

    version

--- githubtarea1/src/progra_taller/rnegocio/dao/IUsuario.java
+++ githubtarea1/src/progra_taller/rnegocio/dao/IUsuario.java
@@ -3,7 +3,11 @@ import progra_taller.rnegocio.entidades.*;
 import java.util.List;
 
 public interface IUsuario {
+    
+    
      public int insertar(Usuario usuario) throws Exception;
      public List<Usuario> obtener() throws Exception;
      public Usuario obtener(int codigo) throws Exception;   
+     
+     
 }
