813d1a9c269b4daebe2f6a3d7d46c9abee238a47
Fabricio <Usuario@172.30.3.144>
2018-01-16 14:59:02 -0500

    version1.1

--- githubtarea1/src/progra_taller/rnegocio/dao/IEtiqueta.java
+++ githubtarea1/src/progra_taller/rnegocio/dao/IEtiqueta.java
@@ -3,9 +3,12 @@ import progra_taller.rnegocio.entidades.*;
 import java.util.List;
 
 public interface IEtiqueta {
+    
      public int insertar(Etiqueta etiqueta) throws Exception;
      
      public List<Etiqueta> obtener() throws Exception;
+     
+     
      public Etiqueta obtener(int codigo) throws Exception;   
      
 }
